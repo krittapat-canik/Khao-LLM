76c3bbd841e4f22d084cd130de86b193a0575b0f
krittpat Canik <krittpat.phonnchuenprapada@gmail.com>
2023-06-27 05:04:10 +0700

    DATA: earn added

--- Khao/src/freefrom-train.py
+++ Khao/src/freefrom-train.py
@@ -62,9 +62,8 @@ def smart_tokenizer_and_embedding_resize(
     tokenizer: transformers.PreTrainedTokenizer,
     model: transformers.PreTrainedModel,
 ):
-    """Resize tokenizer and embedding.
-
-    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+    """
+    Resize tokenizer and embedding.
     """
     num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
     model.resize_token_embeddings(len(tokenizer))
